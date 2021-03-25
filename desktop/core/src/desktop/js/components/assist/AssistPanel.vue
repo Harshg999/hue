b518a20c65589fca721f8e90b58ad09bfc70630e
Johan Ahlen <johan@johanahlen.com>
2021-03-25 14:07:36 +0100

    [frontend] Skeleton for the new assist panel

---
+++ desktop/core/src/desktop/js/components/assist/AssistPanel.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <SqlAssistPanel />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import SqlAssistPanel from './SqlAssistPanel.vue';
+
+  export default defineComponent({
+    name: 'AssistPanel',
+    components: { SqlAssistPanel },
+    setup() {
+      return {};
+    }
+  });
+</script>
---
+++ desktop/core/src/desktop/js/components/assist/SqlAssistPanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <SqlConnectorDropdown v-model="activeConnector" />
+  </div>
+  {{ activeConnector?.name }}
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+
+  import SqlConnectorDropdown from 'components/SqlConnectorDropdown.vue';
+  import { EditorInterpreter } from 'config/types';
+  import I18n from 'utils/i18n';
+
+  export default defineComponent({
+    name: 'SqlAssistPanel',
+    components: { SqlConnectorDropdown },
+    setup() {
+      const activeConnector = ref<EditorInterpreter | null>();
+
+      return { activeConnector, I18n };
+    }
+  });
+</script>
